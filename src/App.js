edd8a8cb8dd7ab1e87d1741664014e8eda7423a0
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-28 15:04:30 -0700

    created file to hold dummy data and filled it with dummy rating comments

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import Header from './components/Header';
 import FeedbackItem from './components/FeedbackItem';
-
+import FeedbackData from './data/FeedbackData';
 const App = () => {
+
+    const [feedback, setFeedback] = useState(FeedbackData)
+
   return (
     <>
         <Header  />
