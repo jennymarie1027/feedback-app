79aa6de90410c13bb4a57d7a48cb2b96f2a3ce3b
jennymarie1027 <jennymarie1027@gmail.com>
2022-01-31 14:12:47 -0700

    installed React icons to get access to the font awesome library

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,13 +1,19 @@
-import Card from './shared/Card';
 import PropTypes from 'prop-types'
+import { FaTimes } from 'react-icons/fa';
 
-function FeedbackItem({ item }) {
-  return <Card>
+function FeedbackItem({ item, handleDelete }) {
+
+
+
+  return <div className='card'>
       <div className="num-display">{item.rating}</div>
+      <button onClick={() => handleDelete(item.id)} className='close'>
+          <FaTimes color='purple' />
+      </button>
       <div className="text-display">
           {item.text}
       </div>
-  </Card>;
+  </div>;
 }
 
 FeedbackItem.propTypes = {
