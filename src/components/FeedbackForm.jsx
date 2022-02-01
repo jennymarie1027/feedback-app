3ec2334a32b54535d632e54dd788453cc466e422
jennymarie1027 <jennymarie1027@gmail.com>
2022-02-01 15:00:40 -0700

    embed the RatingSelect.jsx component into the FeedbackForm and pass a function into the select component that will handle how rating is passed in to the onSubmit

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,10 +1,12 @@
-import Card from './shared/Card'
 import {useState} from 'react';
+import Card from './shared/Card'
 import Button from './shared/Button'
+import RatingSelect from './RatingSelect';
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
 
     const [text, setText] = useState('');
+    const [rating, setRating] = useState(10);
     const [btnDisabled, setBtnDisabled] = useState(true)
     const [message, setMessage] = useState('')
 
@@ -22,10 +24,23 @@ function FeedbackForm() {
         setText(e.target.value);
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (text.trim().length > 10) {
+            const newFeedback = {
+                text,
+                rating
+            }
+
+            handleAdd(newFeedback)
+            setText('');
+        }
+    }
+
   return <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
           <h2>How would you rate your service with us?</h2>
-          {/* TODO - rating select component */}
+          <RatingSelect select={(rating) => setRating(rating)} />
           <div className='input-group'>
               <input type='text' value={text} onChange={handleTextChange} placeholder='write a review' />
               <Button type='submit' isDisabled={btnDisabled} >Send</Button>
