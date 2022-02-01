e5684c017f613f8f4fe647250883d347ad1f691a
jennymarie1027 <jennymarie1027@gmail.com>
2022-02-01 13:52:16 -0700

    disable btn & display message if text.length is not greater than 10 chars

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -5,8 +5,20 @@ import Button from './shared/Button'
 function FeedbackForm() {
 
     const [text, setText] = useState('');
+    const [btnDisabled, setBtnDisabled] = useState(true)
+    const [message, setMessage] = useState('')
 
     function handleTextChange(e) {
+        if (text === '') {
+            setBtnDisabled(true);
+            setMessage(null);
+        } else if (text !== '' && text.trim().length <= 10) {
+            setBtnDisabled(true);
+            setMessage('Text must be at least 10 characters')
+        } else {
+            setMessage(null);
+            setBtnDisabled(false);
+        }
         setText(e.target.value);
     }
 
@@ -16,8 +28,9 @@ function FeedbackForm() {
           {/* TODO - rating select component */}
           <div className='input-group'>
               <input type='text' value={text} onChange={handleTextChange} placeholder='write a review' />
-              <Button type='submit'  >Send</Button>
+              <Button type='submit' isDisabled={btnDisabled} >Send</Button>
           </div>
+          {message && <div className='message'>{message}</div>}
       </form>
   </Card>;
 }
