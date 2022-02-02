7ce66e358ab64139be19047a9b453179c881f6eb
jennymarie1027 <jennymarie1027@gmail.com>
2022-02-02 15:17:05 -0700

    Link aboutPage to FeedbackForm

--- src/components/pages/AboutPage.jsx
+++ src/components/pages/AboutPage.jsx
@@ -1,4 +1,5 @@
 import Card from '../shared/Card'
+import { Link } from 'react-router-dom'
 
 function AboutPage() {
   return (
@@ -8,7 +9,7 @@ function AboutPage() {
             <p>This is a react app to leave feedback for a product or service</p>
             <p>Version: 1.0.0</p>
             <p>
-                <a href='/'>Back to home</a>
+                <Link to='/'>Back to home</Link>
             </p>
         </div>
     </Card>
