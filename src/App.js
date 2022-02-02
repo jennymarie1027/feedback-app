acfefce2bc9cc25140bbce19ebf3938a5e44b016
jennymarie1027 <jennymarie1027@gmail.com>
2022-02-02 15:11:38 -0700

    created AboutPage component, established routing using react-router-dom version 6 with links to aboutPage and another to feedback components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './components/pages/AboutPage';
+
 import { v4 as uuidv4} from 'uuid';
 
 const App = () => {
@@ -22,14 +25,23 @@ const App = () => {
     }
 
   return (
-    <>
+    <Router>
         <Header  />
         <div className='container'>
-          <FeedbackForm handleAdd={addFeedback}/>
-          <FeedbackStats feedback={feedback} />
-          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+          <Routes>
+            <Route path='/' element={
+              <>
+                <FeedbackForm handleAdd={addFeedback}/>
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+              </>
+            }>
+
+            </Route>
+            <Route path='/about' element={<AboutPage/>}/>
+          </Routes>
         </div>
-    </>
+    </Router>
   );
 };
 
