1cff601222f4f18c9ab05b04e132e3e8c9aeb5e5
jennymarie1027 <jennymarie1027@gmail.com>
2022-02-04 15:00:35 -0700

    hooked up addFeedback to json server so data persists

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -25,8 +25,9 @@ export const FeedbackProvider = ({children}) => {
     }
 
     // handle deleting feedback
-    function deleteFeedback(id) {
+    async function deleteFeedback(id) {
         if (window.confirm('Are you sure you want to delete?')) {
+            await fetch(`/feedback/${id}`, { method: 'DELETE'})
           setFeedback(feedback.filter((item) => item.id !== id));
         }
       }
@@ -54,8 +55,16 @@ export const FeedbackProvider = ({children}) => {
         setFeedback([data, ...feedback])
       }
 
-      const updateFeedback = (id, updItem) => {
-        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...updItem } : item))
+      const updateFeedback = async (id, updItem) => {
+          const response = await fetch(`/feedback/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updItem)
+          })
+          const data = await response.json();
+        setFeedback(feedback.map((item) => (item.id === id ? {...item, ...data } : item))
         )
       }
 
